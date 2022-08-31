58087ff13ce73e679393b6f0f28127f3e2154c2c
varnavskysv <sergei.varnavsky@ocrv.ru>
2022-08-31 16:09:55 +0700

    Модель данных.

---
+++ src/main/java/com/company/task4jmix/entity/SheetMark.java
@@ -0,0 +1,45 @@
+package com.company.task4jmix.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "SHEET_MARK")
+@Entity
+public class SheetMark {
+    @Column(name = "ID", nullable = false)
+    @Id
+    @InstanceName
+    private Integer id;
+
+    @JoinColumn(name = "TEACHER_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Teacher teacher;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+}
---
+++ src/main/java/com/company/task4jmix/entity/Teacher.java
@@ -0,0 +1,44 @@
+package com.company.task4jmix.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.Composition;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "TEACHER")
+@Entity
+public class Teacher {
+
+
+    @Id
+    @Column(name="NAME", nullable = false)
+    private String name;
+
+    @Composition
+    @OneToMany(mappedBy = "teacher")
+    private List<SheetMark> sheetMarkList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SheetMark> getSheetMarkList() {
+        return sheetMarkList;
+    }
+
+    public void setSheetMarkList(List<SheetMark> sheetMarkList) {
+        this.sheetMarkList = sheetMarkList;
+    }
+}
